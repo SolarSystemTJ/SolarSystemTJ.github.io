68e7d514b09a616acc1da508914c54a46dc9e57b
Авазбек <93321323+avazbekAko@users.noreply.github.com>
2023-01-04 20:55:26 +0500

    Delete public directory

--- public/llib.js
+++
@@ -1,4 +0,0 @@
-// import * as THREE from "E:/This folder/Системаи Офтобӣ/J/snode_modules/three/build/three.module.js";
-// import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
-
-// import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
